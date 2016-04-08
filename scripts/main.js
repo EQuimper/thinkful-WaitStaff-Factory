f23dd8c5f8205c7c7a04b8bd5f5c455f7041ea39
EQuimper <quimperemanuel@gmail.com>
2016-04-08 12:44:20 -0600

    Add all function now full working

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,36 @@
 var myApp = angular.module('myApp', ['ngMaterial']);
 
 myApp.controller('DetailsCtrl', function ($scope) {
-    $scope.getSubTotal = function (a, b) {
-        return (a + ((a * b) / 100));
-    };
-    
-    $scope.getTip = function (a, b) {
-        return ((a * b) / 100);
-    };
+   var count = 0;
 
-    $scope.getTotal = function (a, b) {
-        return (a + b);
-    };
+    $scope.submit = function () {
+        count++;
+
+        $scope.data.subTotal = $scope.data.baseMeal * (1 + $scope.data.taxRate / 100);
+        $scope.data.tipCalc = $scope.data.subTotal * ($scope.data.tipPerc / 100);
+        $scope.data.totalCalc = $scope.data.subTotal + $scope.data.tipCalc;
+
+        $scope.data.tipTotal += $scope.data.tipCalc;
+        $scope.data.mealTotal = count;
+        $scope.data.avgTip = $scope.data.tipTotal / $scope.data.mealTotal;
+    }
+
+    $scope.reset = function () {
+        $scope.data = 0;
+        count = 0;
+    }
 });
+
+
+//
+// $scope.getSubTotal = function (a, b) {
+//     return (a + ((a * b) / 100));
+// };
+//
+// $scope.getTip = function (a, b) {
+//     return ((a * b) / 100);
+// };
+//
+// $scope.getTotal = function (a, b) {
+//     return (a + b);
+// };
